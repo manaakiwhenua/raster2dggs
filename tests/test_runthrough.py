fc99e1b128d8ab4e8cd4a6eb4a88ce81543ca33a
ndemaio <nicoletta.demaio@protonmail.ch>
2025-09-17 15:09:23 +0200

    Added the failure messages to the individual smoke tests

--- tests/test_runthrough.py
+++ tests/test_runthrough.py
@@ -35,85 +35,70 @@ class TestRunthrough(TestCase):
 
 class TestH3(TestRunthrough):
     '''
-    Test case description here
+    Sends the test data file through H3 indexing using default parameters.
     '''
     
     
     def test_h3_run(self):
         try:
             #TODO: run h3 with standard test data
-            #TODO first: raise artificial exception to develop except block
-            print("Called TestH3.test_h3_run")
+            raise NotImplementedError()
         
-        except Exception as e:
-            msg = "H3 runthrough failed with Exception.\n" #TODO: assemble message
-            
-            self.fail(msg)
+        except Exception:
+            self.fail(f"TestH3.test_h3_run: H3 runthrough failed.")
     
 
 class TestRHP(TestRunthrough):
     '''
-    Test case description here
+    Sends the test data file through rHP indexing using default parameters.
     '''
     
     def test_rhp_run(self):
         try:
             #TODO: run rhp with standard test data
-            #TODO first: raise artificial exception to develop except block
-            print("Called TestRHP.test_rhp_run")
+            raise NotImplementedError()
         
-        except Exception as e:
-            msg = "rHP runthrough failed with Exception.\n" #TODO: assemble message
-            
-            self.fail(msg)
+        except Exception:
+            self.fail(f"TestRHP.test_rhp_run: rHP runthrough failed.")
     
 
 class TestGeohash(TestRunthrough):
     '''
-    Test case description here
+    Sends the test data file through Geohash indexing using default parameters.
     '''
     
     def test_geohash_run(self):
         try:
             #TODO: run geohash with standard test data
-            #TODO first: raise artificial exception to develop except block
-            print("Called TestGeohash.test_geohash_run")
+            raise NotImplementedError()
         
-        except Exception as e:
-            msg = "Geohash runthrough failed with Exception.\n" #TODO: assemble message
-            
-            self.fail(msg)
+        except Exception:
+            self.fail(f"TestGeohash.test_geohash_run: Geohash runthrough failed.")
     
 
 class TestMaidenhead(TestRunthrough):
     '''
-    Test case description here
+    Sends the test data file through Maidenhead indexing using default parameters.
     '''
     
     def test_maidenhead_run(self):
         try:
             #TODO: run maidenhead with standard test data
-            #TODO first: raise artificial exception to develop except block
-            print("Called TestMaidenhead.test_maidenhead_run")
+            raise NotImplementedError()
         
         except Exception as e:
-            msg = "Maidenhead runthrough failed with Exception.\n" #TODO: assemble message
-            
-            self.fail(msg)
+            self.fail(f"TestMaidenhead.test_maidenhead_run: Maidenhead runthrough failed.")
     
 
 class TestS2(TestRunthrough):
     '''
-    Test case description here
+    Sends the test data file through S2 indexing using default parameters.
     '''
     
     def test_s2_run(self):
         try:
             #TODO: run s2 with standard test data
-            #TODO first: raise artificial exception to develop except block
-            print("Called TestS2.test_s2_run")
+            raise NotImplementedError()
         
-        except Exception as e:
-            msg = "S2 runthrough failed with Exception.\n" #TODO: assemble message
-            
-            self.fail(msg)
+        except Exception:
+            self.fail(f"TestS2.test_s2_run: S2 runthrough failed.")
