14b25f4b4fa4593d6d214bcac1435718b6cc688a
ndemaio <nicoletta.demaio@protonmail.ch>
2025-09-17 12:51:24 +0200

    Created a test runner script and a file for data paths used for testing

---
+++ tests/data/datapaths.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+TEST_FILE_PATH = Path("data/se-island.tiff")
+TEST_OUTPUT_PATH = Path("data/output/")
---
+++ tests/test_raster2dggs.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+if __name__ == "__main__":
+    testSuite = unittest.defaultTestLoader.discover(start_dir=".", top_level_dir="..")
+    testRunner = unittest.TextTestRunner()
+    testRunner.run(testSuite)    
