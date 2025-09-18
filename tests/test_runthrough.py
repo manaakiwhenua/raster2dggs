715a13cfdbdf6ebc9e976c3a7bec32526fc49218
ndemaio <nicoletta.demaio@protonmail.ch>
2025-09-18 10:42:09 +0200

    Changed the resolution for the Maidenhead smoke test to 3 (following PR comment)

--- tests/test_runthrough.py
+++ tests/test_runthrough.py
@@ -83,7 +83,7 @@ class TestMaidenhead(TestRunthrough):
     
     def test_maidenhead_run(self):
         try:
-            maidenhead([TEST_FILE_PATH, str(TEST_OUTPUT_PATH), "-r", "6"], standalone_mode = False)
+            maidenhead([TEST_FILE_PATH, str(TEST_OUTPUT_PATH), "-r", "3"], standalone_mode = False)
         
         except Exception as e:
             self.fail(f"TestMaidenhead.test_maidenhead_run: Maidenhead runthrough failed.")
