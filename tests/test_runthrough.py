9ffa1910561d0f2beace90d00ec018e0a4118a39
Richard Law <lawr@landcareresearch.co.nz>
2025-12-09 23:30:30 +1300

    restores full tests

--- tests/test_runthrough.py
+++ tests/test_runthrough.py
@@ -2,11 +2,11 @@
 @author: ndemaio
 """
 
-# from classes.h3 import TestH3
-# from classes.rHP import TestRHP
-# from classes.geohash import TestGeohash
-# from classes.maidenhead import TestMaidenhead
-# from classes.s2 import TestS2
+from classes.h3 import TestH3
+from classes.rHP import TestRHP
+from classes.geohash import TestGeohash
+from classes.maidenhead import TestMaidenhead
+from classes.s2 import TestS2
 from classes.a5 import TestA5
-# from classes.isea9r import TestISEA9R
-# from classes.iseah7 import TestISEA7H
+from classes.isea9r import TestISEA9R
+from classes.iseah7 import TestISEA7H
